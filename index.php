e9676c7e12ebff45a2055a52d16bc9e1eaffadf1
David Casagrande <dncasagrande@gmail.com>
2015-03-16 11:22:05 -0400

    pull all portfolio items into the index template and added template to render custom style classes for portfolio categories

--- index.php
+++ index.php
@@ -8,8 +8,34 @@
     <?php include('templates/logo.php'); ?>
     <?php include('templates/main-nav.php'); ?>
     <div class="content large-7 columns">
-      <div class="narrative">
-        What up don
+      <div class="portfolio">
+        <?php
+          $args = array('post_type'=> 'portfolio');
+          $portfolio = query_posts($args);
+
+          echo "<ul class=\"small-block-grid-2 medium-block-grid-2 large-block-grid-2\">";
+            foreach($portfolio as $item) {
+              $thumbnail = get_field('thumbnail', $item->ID);
+              $title = get_field('title', $item->ID);
+              $url = get_permalink($item->ID);
+              $categories = get_the_terms($item->ID, 'portfolio-category');
+              $category_slug = "no-category";
+              if($categories) {
+                $category = reset($categories);
+                $category_slug = $category->slug;
+              }
+
+              echo "<li class=\"{$category_slug}\">";
+                echo "<div class=\"portfolio-item position-relative\">";
+                  echo "<a href=\"{$url}\" class=\"position-relative\">";
+                    echo "<img src=\"{$thumbnail['url']}\" alt=\"{$title}\" />";
+                  echo "</a>";
+                  // echo "<div class=\"title\">{$title}</div>";
+                echo "</div>";
+              echo "</li>";
+            }
+          echo "</ul>";
+        ?>
       </div>
     </div>
   </div>
--- templates/portfolio_categories_css.php
+++ templates/portfolio_categories_css.php
@@ -7,9 +7,13 @@
     foreach($portfolio_categories as $category) {
       $slug = $category->slug;
       $color = $category->description;
+      $name = $category->name;
+      print_r($category);
       if($slug && $color) {
         echo ".{$slug} .header { background-color: {$color} !important; }";
         echo ".{$slug} a { color: {$color} !important; }";
+        echo ".{$slug} .portfolio-item { border-color: {$color} !important; }";
+        echo ".{$slug} .portfolio-item:after { background-color: {$color} !important; content: '{$name}'; }";
       }
     }
   echo "</style>";
