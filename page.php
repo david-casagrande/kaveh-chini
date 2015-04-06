7837384cfa3463f0b7290c72482bd9d8a38fcea4
David Casagrande <dncasagrande@gmail.com>
2015-04-06 10:05:53 -0400

    added page template and page-narrative class with matching color to nav

--- page.php
+++ page.php
@@ -1,5 +1,25 @@
 <?php get_header(); ?>
 
-<?php include('templates/page.php'); ?>
+<?php
+  $kaveh_chini_classes = array('kaveh-chini', 'row');
+?>
+<div class="main-view">
+  <div class="main-view-table">
+    <?php include('templates/logo.php'); ?>
+    <?php include('templates/main_nav.php'); ?>
+    <div class="content display-table-cell">
+      <?php
+        if ( have_posts() ) {
+          while ( have_posts() ) {
+            the_post();
+            echo "<div class=\"page-narrative\">";
+            the_content();
+            echo "</div>";
+          }
+        }
+      ?>
+    </div>
+  </div>
+</div>
 
 <?php get_footer(); ?>
